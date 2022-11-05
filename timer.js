70d2652ea009f5b08a0aae044c4a5f4c8bf7c594
FLCorp <108215519+FLCorp@users.noreply.github.com>
2022-11-05 09:13:56 +0100

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,17 @@
+const departMinutes = 60
+let temps = departMinutes * 60
+
+const timerElement = document.getElementById("weekly")
+
+setInterval(() => {
+    let heures = parseInt(temps / 60, 10)
+    let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
+
+    heures = heures < 10 ? "0" + heures : heures
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+
+    timerElement.innerText = `${heures}:${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+}, 1000)
